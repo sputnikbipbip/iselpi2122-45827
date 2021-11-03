72f416cecab9135791a6d56b02b457a74d4a8c7e
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2021-11-03 22:56:56 +0000

    async not working

--- Part1/part2.js
+++ Part1/part2.js
@@ -1,10 +1,10 @@
 'use strict'
 
+/*
 const fs = require('fs')
 const fetch = require("node-fetch")
-
-
 const client_id = process.env.ATLAS_CLIENT_ID;
+
 
 function urlBuilder () {
     return new Promise((resolve, reject) => {
@@ -37,9 +37,37 @@ urlBuilder()
     .catch(function(error) {
         console.log('There has been a problem with your fetch operation: ' + error.message);
 })
+*/
 
-    
+const fs = require('fs').promises
+const fetch = require("node-fetch")
+const client_id = process.env.ATLAS_CLIENT_ID;
 
 
+async function inspectorGadget () {
+    try {
+        const url = await fs.readFile('./id.txt', 'utf8', (err, data) => {
+            if (err) {
+                return console.log(err)
+            }
+            const gamesIds = data.split("\r\n")
+            return `https://api.boardgameatlas.com/api/search?ids=${gamesIds.join(",")}&client_id=${client_id}`
+        })
+        const data = await fetch(url)
+        const parsedData = await data.json()
+        const array = (parsedData) => parsedData.games.map((game) => {
+            return {
+                id : game.id,
+                name : game.name,
+                url : game.url
+            }
+        })
+        console.log(array)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+inspectorGadget()
 
 
