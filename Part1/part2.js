5010bd11736aec4f7402811ee2c3a31cdf4776fe
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2021-11-03 21:40:02 +0000

    promises done

--- Part1/part2.js
+++ Part1/part2.js
@@ -3,35 +3,43 @@
 const fs = require('fs')
 const fetch = require("node-fetch")
 
-let gamesIds
-let idsString
 
 const client_id = process.env.ATLAS_CLIENT_ID;
-const URL = `https://api.boardgameatlas.com/api/search?ids=${idsString}&client_id=${client_id}`
 
+function urlBuilder () {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./id.txt', 'utf8', (err, data) => {
+            if (err) {
+                reject(err)
+            }
+            const gamesIds = data.split("\r\n")
+            resolve(`https://api.boardgameatlas.com/api/search?ids=${gamesIds.join(",")}&client_id=${client_id}`)
+        })
+    }) 
+}
 
-
-fs.readFile('./id.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    gamesIds = data.split("\n")
-    gamesIds.forEach(id => {
-        idsString += `${id},`
-    })
-    idsString.slice(-1)
-    console.log(gamesIds)
-    console.log(idsString)
-})
-
-
-fetch(URL)                                              
-    .then(rsp => rsp.json())  
-    .then(data => console.log(data))                         
+urlBuilder()
+    .then(url => {
+        console.log(`url is : ${url}`)
+        return fetch(url)}
+    )
+    .then(data => data.json())
+    .then(
+        data => data.games.map(function(game) {
+            return {
+                id : game.id,
+                name : game.name,
+                url : game.url
+            }
+        })
+    )
+    .then(array => console.log(array))                         
     .catch(function(error) {
         console.log('There has been a problem with your fetch operation: ' + error.message);
-      });
+})
+
+    
+
 
 
 
