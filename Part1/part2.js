b54fe084b9f3f13f0c56b843fab886aeb1d642a0
Daniel Azevedo <A45827@alunos.isel.pt>
2021-11-03 16:58:51 +0000

    slight update

--- Part1/part2.js
+++ Part1/part2.js
@@ -1,10 +1,30 @@
+'use strict'
 
+const fs = require('fs')
 const fetch = require("node-fetch")
 
-const client_id = process.env.ATLAS_CLIENT_ID;
-const URL = `https://api.boardgameatlas.com/api/lists?username=trentellingsen&client_id=${client_id}`
+let gamesIds
+let idsString
 
-const result = []
+const client_id = process.env.ATLAS_CLIENT_ID;
+const URL = `https://api.boardgameatlas.com/api/search?ids=${idsString}&client_id=${client_id}`
+
+
+
+fs.readFile('./id.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    gamesIds = data.split("\n")
+    gamesIds.forEach(id => {
+        idsString += `${id},`
+    })
+    idsString.slice(-1)
+    console.log(gamesIds)
+    console.log(idsString)
+})
+
 
 fetch(URL)                                              
     .then(rsp => rsp.json())  
@@ -12,7 +32,6 @@ fetch(URL)
     .catch(function(error) {
         console.log('There has been a problem with your fetch operation: ' + error.message);
       });
-
 
 
 
